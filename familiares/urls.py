43d5c116ae303e7364df57082f556cc13b2f1f75
Massimiliano <cirinomaximiliano@gmail.com>
2022-08-28 21:17:48 +0200

    familiares

---
+++ familiares/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from familiares.views import probar_template
+
+urlpatterns = [
+    path('', probar_template),
+]
--- familiares/views.py
+++ familiares/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template, Context, loader
+from familiares.models import per_familia
 
-# Create your views here.
+def probar_template(request):
+    queryset = per_familia.objects.all()
+    diccionario = {'familiares': queryset}
+    plantilla = loader.get_template( 'familiares_list.html')
+    documento_html = plantilla.render(diccionario)
+
+    return HttpResponse(documento_html)
